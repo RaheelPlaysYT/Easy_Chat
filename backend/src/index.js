9498851570dfd50006a9b7c97619dc223da748f7
Adeel Shaheen <madeelshaheen27@gmail.com>
2026-01-17 21:09:11 +0500

    feat: update package.json to include Capacitor dependencies and add vercel.json for deployment configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import path from "path";
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -16,13 +16,16 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        process.env.CLIENT_URL
+    ],
     credentials: true
 }));
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
-if (process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const axiosInstance = axios.create ({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api": "/api",
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL ? `${import.meta.env.VITE_API_BASE_URL}/api` : "http://localhost:5001/api",
     withCredentials: true,
 });
